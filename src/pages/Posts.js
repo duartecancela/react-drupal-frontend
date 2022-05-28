c6f6f390c539984d5220d426f5d1e908ac92f0df
Duarte Cancela <duartecancela@gmail.com>
2022-05-28 22:39:25 +0100

    update axios

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,29 +1,51 @@
-import React from 'react'
+import React,{useState, useEffect} from "react";
 import axios from "axios";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts";
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
 function Posts(props) {
 
     const [post, setPost] = React.useState(null);
+    const [data, setData] = useState({results:[]});
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
-    React.useEffect(() => {
-        axios.get(`${baseURL}`).then((response) => {
-          setPost(response.data);
-        });
-      }, []);
-
-    if (!post) return "No post!"
-    console.log(post)
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsError(false);
+            setIsLoading(true);
+            try {
+                const result = await axios(URL);
+                setData(result.data);
+            } catch (error) {
+                setIsError(true);
+            }
+            setIsLoading(false);
+        };
+        fetchData();
+    }, []);
+    
+    console.log(data)
 
   return (
-    <div>
-        {post.map(item => (
-            <>
-                <h1>{item.title}</h1> 
-                <p>{item.body}</p>
-            </>
-        ))}
+    <div className="Home">
+        {isError && <div>Something went wrong ...</div>}
+
+        {isLoading ? (
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        ) : 
+        (
+            <div>
+                {data.map(item => (
+                    <>
+                        <h1>{item.title}</h1> 
+                        <p>{item.body}</p>
+                    </>
+                ))}
+            </div>
+        )}
     </div>
   )
 }
